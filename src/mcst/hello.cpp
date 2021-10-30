2a1a7acaa6224cf5a06fe93255905a2f5a55bdb3
NNNeil-C <ex_lunatic@hotmail.com>
2021-10-30 17:59:09 +0800

    add hello.cpp

---
+++ src/mcst/hello.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: Neil.Chen Zifeng 
+ * @Date: 2021-10-30 17:55:56 
+ * @Last Modified by: Neil.Chen Zifeng
+ * @Last Modified time: 2021-10-30 17:57:30
+ */
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world!" << endl;
+}
