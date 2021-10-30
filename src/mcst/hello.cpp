2e6d5979222860ec3a60fcc8ba85eaa7e8dc5677
NNNeil-C <ex_lunatic@hotmail.com>
2021-10-30 18:21:48 +0800

    save

--- src/mcst/hello.cpp
+++ src/mcst/hello.cpp
@@ -2,7 +2,7 @@
  * @Author: Neil.Chen Zifeng 
  * @Date: 2021-10-30 17:55:56 
  * @Last Modified by: Neil.Chen Zifeng
- * @Last Modified time: 2021-10-30 18:03:46
+ * @Last Modified time: 2021-10-30 18:21:23
  */
 #include<iostream>
 using namespace std;
