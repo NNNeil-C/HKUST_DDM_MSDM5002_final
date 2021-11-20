b1e8c86f2bc8429c2b3a83420942de71b1a95aa1
Cynthia-Guan <cynthia_gym@163.com>
2021-11-20 23:19:27 +0800

    some modifications of the structure & bug fix

--- src/UI.py
+++ src/UI.py
@@ -4,26 +4,31 @@ import numpy as np
 
 class Piece:
 
-    def __init__(self, x, y, gamer):
-        self.x = x
-        self.y = y
-        self.gamer = gamer
+    def __init__(self):
+        self.__row = None
+        self.__col = None
+        self.__gamer = None
+
+    def set_pos(self, x: int, y: int):
+        self.__row = x
+        self.__col = y
 
     def get_pos(self):
+        return self.__row, self.__col
 
-        return self.x, self.y
+    def set_color(self, color: int):
+        self.__gamer = color
 
     def get_color(self):
-        if self.gamer == 1:
-            return 'black'
-        elif self.gamer == -1:
-            return 'white'
+        if self.__gamer == 1:
+            return 1
+        elif self.__gamer == -1:
+            return -1
         else:
             return False
 
 
 class GameBoard:
-
     def __init__(self, width=8, height=8):
         self.width = width
         self.height = height
@@ -37,18 +42,11 @@ class GameBoard:
             pygame.draw.line(screen, [0, 0, 0], [25, 25 + 50 * x], [size * 50 - 25, 25 + 50 * x], 1)
         pygame.display.update()
 
-    def place_piece(self, piece: Piece):
-        self.board[piece.get_pos()] = piece.gamer
-
-    def place_piece(self, piece: Piece):
-        self.board[piece.get_pos()] = piece.gamer
-
-    def update_ui(self, screen, x=8, y=8):
+    def update_ui(self, screen):
         black_color = [0, 0, 0]
         white_color = [255, 255, 255]
-        d = int(560 / 7)
-        for i in range(x):
-            for j in range(y):
+        for i in range(self.width):
+            for j in range(self.height):
                 pos = [50 * i + 25, 50 * j + 25]
                 if self.board[i][j] == 1:
                     pygame.draw.circle(screen, black_color, pos, 18, 0)
@@ -56,20 +54,24 @@ class GameBoard:
                     pygame.draw.circle(screen, white_color, pos, 18, 0)
         pygame.display.update()
 
+    def place_piece(self, piece: Piece):
+        self.board[piece.get_pos()] = piece.get_color()
 
-if __name__ == '__main__':
-    pygame.init()
-    M = 12
-    piece = Piece(x=11, y=11, gamer=1)
-    screen = pygame.display.set_mode((50 * M, 50 * M))
-    pygame.display.set_caption('Interface of Five-in-a-Row')
-    game = GameBoard(M, M)
-    game.ini_screen(screen=screen)
-    game.place_piece(piece)
-    print(game.board)
-    game.update_ui(screen=screen)
-    done = False
-    while not done:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                done = True
+
+
+# if __name__ == '__main__':
+#     pygame.init()
+#     M = 12
+#     piece = Piece(row=11, col=11, gamer=1)
+#     screen = pygame.display.set_mode((50 * M, 50 * M))
+#     pygame.display.set_caption('Interface of Five-in-a-Row')
+#     game = GameBoard(M, M)
+#     game.ini_screen(screen=screen)
+#     game.place_piece(piece)
+#     print(game.board)
+#     game.update_ui(screen=screen)
+#     done = False
+#     while not done:
+#         for e in pygame.event.get():
+#             if e.type == pygame.QUIT:
+#                 done = True
--- src/function_realize.py
+++ src/function_realize.py
@@ -1,111 +1,106 @@
 '''
 Author: Cynthia G
-Last Edit: 2021/11/17 11:33
+Fist Edit: 2021/11/13
+Last Edit: 2021/11/20 23:13:47
 
 Work remained by far: Reformation of classes
-                      Modification of board update function
+                      Modification of board update method
                       Include the situation that mouse click off board
                       Message showing the winner
-
+                      Interface for AI
 '''
 
 import pygame
 import UI
 
 
-# class Piece:
-#     def __init__(self):
-#         self.__color = 1
-#
-#     def set_color(self, color: int):
-#         self.__color = color
-#
-#     def get_color(self):
-#         if self.__color == 'black':
-#             return [0, 0, 0]
-#         if self.__color == 'white':
-#             return [255, 255, 255]
+class HumanPlayer:
+    def __init__(self):
+        self.piece = UI.Piece()
 
-# class
-
-def update_by_man(event):
-    """
-    This method detects the mouse click on the game window. Update the state matrix of the game.
-    input:
-        event:pygame event, which are either quit or mouse click)
-    """
-
-    x, y = event.pos
-    row = round((y - 25) / 50)
-    col = round((x - 25) / 50)
-    piece = UI.Piece(col, row, 1)
-    return piece
+    def update_by_human(self, event):
+        x, y = event.pos
+        row = round((x - 25) / 50)
+        col = round((y - 25) / 50)
+        self.piece.set_pos(row, col)
 
 
+class AIPlayer:
+    def __init__(self):
+        self.piece = UI.Piece()
 
-# def update_by_machine(self):
-#     pygame.event.set_blocked(None)
-#     position = Gamer.find_best_action()
-#     pygame.event.set_allowed([pygame.QUIT, pygame.MOUSEBUTTONDOWN])
-#     x, y = position
-#     piece = UI.Piece(x, y, -1)
-#     return piece
+    # def update_by_machine(self):
+    #     pygame.event.set_blocked(None)
+    #     position = Gamer.find_best_action()
+    #     pygame.event.set_allowed([pygame.QUIT, pygame.MOUSEBUTTONDOWN])
+    #     x, y = position
+    #     self.piece = UI.Piece(x, y, -1)
 
 
-def check_for_done(mat, piece):
-    up = (0, 1)
-    down = (0, -1)
-    lft = (-1, 0)
-    rgt = (1, 0)
-    up_lft = (-1, 1)
-    up_rgt = (1, 1)
-    down_lft = (-1, -1)
-    down_rgt = (1, -1)
+class CheckWinning:
+    def __init__(self):
+        self.done = False
 
-    vertical = __count_pieces(mat, piece, up) + __count_pieces(mat, piece, down) - 1
-    if vertical == 5:
-        return True
+    def check(self, mat: UI.GameBoard, piece: UI.Piece):
+        up = (0, 1)
+        down = (0, -1)
+        lft = (-1, 0)
+        rgt = (1, 0)
+        up_lft = (-1, 1)
+        up_rgt = (1, 1)
+        down_lft = (-1, -1)
+        down_rgt = (1, -1)
 
-    horizontal = __count_pieces(mat, piece, lft) + __count_pieces(mat, piece, rgt) - 1
-    if horizontal == 5:
-        return True
+        vertical = self.__count_pieces(mat, piece, up) + self.__count_pieces(mat, piece, down) - 1
+        horizontal = self.__count_pieces(mat, piece, lft) + self.__count_pieces(mat, piece, rgt) - 1
+        diagonal = self.__count_pieces(mat, piece, up_lft) + self.__count_pieces(mat, piece, down_rgt) - 1
+        bck_diagonal = self.__count_pieces(mat, piece, up_rgt) + self.__count_pieces(mat, piece, down_lft) - 1
 
-    diagonal = __count_pieces(mat, piece, up_lft) + __count_pieces(mat, piece, down_rgt) - 1
-    if diagonal == 5:
-        return True
+        if 5 in [vertical, horizontal, diagonal, bck_diagonal]:
+            self.done = True
 
-    bck_diagonal = __count_pieces(mat, piece, up_rgt) + __count_pieces(mat, piece, down_lft) - 1
-    if bck_diagonal == 5:
-        return True
+    def __count_pieces(self, mat: UI.GameBoard, piece: UI.Piece, direct):
+        count = 0
+        x, y = piece.get_pos()
 
-    return False
+        while 0 <= x < mat.width and 0 <= y < mat.height and mat.board[x][y] == piece.get_color():
+            count += 1
+            x, y = x + direct[0], y + direct[1]
+        return count
 
-def __count_pieces(mat: UI.GameBoard, piece: UI.Piece, direct):
-    count = 0
-    x, y = piece.get_pos()
 
-    while 0 <= x < mat.width and 0 <= y < mat.height and mat.board[x][y] == piece.gamer:
-        count += 1
-        x, y = x + direct[0], y + direct[1]
-    return count
+class Game:
+    def __init__(self, fst: str, size: int):
+        self.player1 = fst
+        self.mat = UI.GameBoard(width=size, height=size)
+        self.winner = None
+
+    def start(self):
+        pygame.init()
+        screen = pygame.display.set_mode((50 * self.mat.width, 50 * self.mat.height))
+        pygame.display.set_caption('Interface of Five-in-a-Row')
+        self.mat.ini_screen(screen)
+        print(self.mat.board)
+        win = CheckWinning()
+        if self.player1 == 'Human':
+            while not win.done:
+                for e in pygame.event.get():
+                    if e.type == pygame.QUIT:
+                        win.done = True
+                    elif e.type == pygame.MOUSEBUTTONDOWN:
+                        human = HumanPlayer()
+                        human.piece.set_color(1)
+                        human.update_by_human(e)
+                        self.mat.place_piece(human.piece)
+                        print(self.mat.board)
+                        win.check(self.mat, human.piece)
+                        if win.done:
+                            self.winner = self.player1
+                        self.mat.update_ui(screen)
+
+
 
 
 if __name__ == '__main__':
-    pygame.init()
-    M = 8
-    screen = pygame.display.set_mode((50 * M, 50 * M))
-    pygame.display.set_caption('Interface of Five-in-a-Row')
-    game = UI.GameBoard(M, M)
-    game.ini_screen(screen)
-    print(game.board)
-    done = False
-    while not done:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                done = True
-            elif e.type == pygame.MOUSEBUTTONDOWN:
-                piece = update_by_man(e)
-                game.place_piece(piece)
-                print(game.board)
-                done = check_for_done(game, piece)
-                game.update_ui(screen, M, M)
+    gomoku = Game('Human', 8)
+    gomoku.start()
